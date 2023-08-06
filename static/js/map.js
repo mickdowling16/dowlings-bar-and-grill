94f248f295b6d87d660bb3b3c8919c177d491527
mickdowling16 <>
2023-08-06 12:34:44 +0000

    update menu.js file to include marker

--- static/js/map.js
+++ static/js/map.js
@@ -1,12 +1,12 @@
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(53.523572, -7.348435),
+        center: new google.maps.LatLng(53.344435, -6.264790),
         zoom: 14,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
     var marker = new google.maps.Marker({
-        position: myCenter,
+        position: new google.maps.LatLng(53.344435, -6.264790),
         animation: google.maps.Animation.BOUNCE
     });
 
